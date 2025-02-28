736b1b9e2dd70885193e2398aa2872459b7bc517
Bina <benjaminbinyam500@gmail.com>
2025-02-28 10:48:04 +0300

    Created a reusable component Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import { tempMovieData } from "./components/data";
+import { tempMovieData, tempWatchedData } from "./components/data";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Logo from "./components/Logo";
 import NumResults from "./components/NumResults";
 import Search from "./components/Search";
-import ListBox from "./components/ListBox";
+import Box from "./components/Box";
 import WatchedBox from "./components/WatchedBox";
 import MovieList from "./components/MovieList";
+import WatchedSummary from "./components/WatchedSummary";
+import WatchedMoviesList from "./components/WatchedMoviesList";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -20,10 +23,13 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );

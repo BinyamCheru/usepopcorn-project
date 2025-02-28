b7bc615079a472318c42711bc4415cfc6ba58ece
Bina <benjaminbinyam500@gmail.com>
2025-02-28 09:55:03 +0300

    Separate the app jsx in to components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+const MainBody = () => {
+
+
+  return (
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+};
+export default MainBody;

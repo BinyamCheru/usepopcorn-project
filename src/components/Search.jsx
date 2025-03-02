7e166659225b1e75721131147e1c2b220335f886
Bina <benjaminbinyam500@gmail.com>
2025-03-02 15:12:14 +0300

    Making the the Search focus for initial render without using reference

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 const Search = ({ query, setQuery }) => {
+  useEffect(() => {
+    const element = document.querySelector(".search");
+    // console.log(element);
+    element.focus();
+  }, []);
+
   return (
     <input
       className="search"
